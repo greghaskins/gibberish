8d2e7d9aca48c19915e9ec727e026fe326b8d81d
Gregory Haskins <greg@greghaskins.com>
2012-07-18 21:23:49 -0400

    Initial commit, from my StackOverflow answer
    See http://stackoverflow.com/a/5502875/356942

---
+++ gibberish.py
@@ -0,0 +1,33 @@
+import string
+import itertools
+import random
+
+initial_consonants = (set(string.ascii_lowercase) - set('aeiou')
+                      # remove those easily confused with others
+                      - set('qxc')
+                      # add some crunchy clusters
+                      | set(['bl', 'br', 'cl', 'cr', 'dr', 'fl',
+                             'fr', 'gl', 'gr', 'pl', 'pr', 'sk',
+                             'sl', 'sm', 'sn', 'sp', 'st', 'str',
+                             'sw', 'tr'])
+                      )
+
+final_consonants = (set(string.ascii_lowercase) - set('aeiou')
+                    # confusable
+                    - set('qxcsj')
+                    # crunchy clusters
+                    | set(['ct', 'ft', 'mp', 'nd', 'ng', 'nk', 'nt',
+                           'pt', 'sk', 'sp', 'ss', 'st'])
+                    )
+
+vowels = 'aeiou' # we'll keep this simple
+
+# each syllable is consonant-vowel-consonant "pronounceable"
+syllables = map(''.join, itertools.product(initial_consonants, 
+                                           vowels, 
+                                           final_consonants))
+
+# you could trow in number combinations, maybe capitalized versions... 
+
+def gibberish(wordcount, wordlist=syllables):
+    return ' '.join(random.sample(wordlist, wordcount))
